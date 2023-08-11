0cf46380b3b57b5ebe8e9ff7b9c8f184a798be38
zhanzhan1848 <270428890@qq.com>
2023-08-11 15:11:16 +0800

    Create a test project

---
+++ Metal_Test/Metal_Test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Metal_Test
+//
+//  Created by imacb on 2023/8/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Metal_Test/Metal_Test/Metal_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metal_TestApp.swift
+//  Metal_Test
+//
+//  Created by imacb on 2023/8/11.
+//
+
+import SwiftUI
+
+@main
+struct Metal_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
