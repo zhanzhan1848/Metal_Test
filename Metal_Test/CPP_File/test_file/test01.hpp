7e897f12c1bd27133a68326c8528430866d24408
zhanzhan1848 <270428890@qq.com>
2023-08-11 16:51:22 +0800

    Add single header file and a test cpp file

--- Metal_Test/CPP_File/test_file/test01.hpp
+++ Metal_Test/CPP_File/test_file/test01.hpp
@@ -8,6 +8,13 @@
 #ifndef test01_hpp
 #define test01_hpp
 
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+
 #include <stdio.h>
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
 
 #endif /* test01_hpp */
